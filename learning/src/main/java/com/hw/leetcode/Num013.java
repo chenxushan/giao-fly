ee8cf6fac4662847d5a64c7968208d2bda44c220
chenxushan <chenxushan58@gmail.com>
2021-07-16 00:00:48 +0800

    num013

--- learning/src/main/java/com/hw/leetcode/Num013.java
+++ learning/src/main/java/com/hw/leetcode/Num013.java
@@ -78,6 +78,9 @@ package com.hw.leetcode;
 
 import com.sun.corba.se.spi.ior.IdentifiableFactory;
 
+import java.util.HashMap;
+import java.util.Map;
+
 //leetcode submit region begin(Prohibit modification and deletion)
 public class Num013 {
 
@@ -170,6 +173,33 @@ public class Num013 {
         }
         return sum;
     }
+
+
+    Map<Character, Integer> symbolValues = new HashMap<Character, Integer>() {{
+        put('I', 1);
+        put('V', 5);
+        put('X', 10);
+        put('L', 50);
+        put('C', 100);
+        put('D', 500);
+        put('M', 1000);
+    }};
+
+    public int romanToInt1(String s) {
+        int ans = 0;
+        int n = s.length();
+        for (int i = 0; i < n; ++i) {
+            int value = symbolValues.get(s.charAt(i));
+            if (i < n - 1 && value < symbolValues.get(s.charAt(i + 1))) {
+                ans -= value;
+            } else {
+                ans += value;
+            }
+        }
+        return ans;
+    }
+
+
 
     public static void main(String[] args) {
         System.out.println(0);
